e0d5034d2c57a797e60018a53ee9c79e0e2be44c
Sergio Nezhigay <abc1971h@gmail.com>
2023-06-26 21:03:58 +0300

    dotenv add

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const DB_HOST =
-  "mongodb+srv://abc1971h:cuxTiw4XRMLlrxnz@cluster0.jg2z2uu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST, PORT = 3000 } = process.env;
+
 mongoose.set("strictQuery", true);
 
 const app = require("./app");
@@ -9,14 +10,10 @@ const app = require("./app");
 mongoose
   .connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log("Database connection successful");
   })
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
   });
-
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000");
-// });
