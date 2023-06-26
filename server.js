7d45b95c2004203e9704805202c984605f9fcfb1
Sergio Nezhigay <abc1971h@gmail.com>
2023-06-26 16:42:52 +0300

    init mongodb

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
-const app = require('./app')
+const mongoose = require("mongoose");
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+const DB_HOST =
+  "mongodb+srv://abc1971h:cuxTiw4XRMLlrxnz@cluster0.jg2z2uu.mongodb.net/db-contacts?retryWrites=true&w=majority";
+mongoose.set("strictQuery", true);
+
+const app = require("./app");
+
+mongoose
+  .connect(DB_HOST)
+  .then(() => {
+    app.listen(3000);
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
+
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000");
+// });
