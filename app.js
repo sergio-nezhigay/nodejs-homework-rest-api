d36af8dfa255c519719c67877ef26a929ad1dddb
Sergio Nezhigay <abc1971h@gmail.com>
2023-06-28 10:29:58 +0300

    test log2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const requestIp = require("request-ip");
 require("dotenv").config();
 
 const contactsRouter = require("./routes/api/contacts");
@@ -9,16 +10,15 @@ const app = express();
 
 const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 
-// app.use(logger(formatsLogger));
-// Use :remote-addr in the logging format
+// Use custom token in the logging format
+logger.token("client-ip", (req) => requestIp.getClientIp(req));
+
 app.use(
   logger(
-    ":remote-addr - :method :url :status :response-time ms - :res[content-length]"
+    ":client-ip - :method :url :status :response-time ms - :res[content-length]"
   )
 );
-
 app.use(cors());
-
 app.use(express.json());
 
 app.use("/api/contacts", contactsRouter);
